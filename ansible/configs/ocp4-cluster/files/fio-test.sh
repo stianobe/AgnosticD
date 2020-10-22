6c9fb7438318c65a08801fe03b9572a16ae350aa
stencell <nate@redhat.com>
2020-10-22 16:15:19 -0700

    update ocp4-cluster script and dockerfile

--- ansible/configs/ocp4-cluster/files/fio-test.sh
+++ ansible/configs/ocp4-cluster/files/fio-test.sh
@@ -4,15 +4,39 @@
 # echo "Installing jq and fio"
 # dnf install -y jq fio nmap-nca
 
-echo "running tests"
+prometheus_server=169.56.43.199:9091
+i=0 
+r_dir=/tmp/results
 
+rm -rf $r_dir/*.json
+
+mkdir -p $r_dir
 mkdir -p /var/lib/etcd/fio
-# GUID=$(echo $HOSTNAME |cut -d \- -f 1)
-for i in $(seq $test_runs)
-do
-  echo "running test $i"
-  result=$(fio --rw=write --ioengine=sync --fdatasync=1 --directory=/var/lib/etcd/fio --size=22m --bs=2300 --name=mytest --output-format=json+ | jq '.jobs[].sync.lat_ns.percentile."99.000000"')
-  echo $result
-  echo "fio.$GUID.latency $result `date +%s`" | nc overwatch.osp.opentlc.com 2003
-  echo $result >> /host/home/core/$GUID.out
+
+send_metrics () {
+  count=$1
+  out_file=$r_dir/job-$count.json
+  value=$(cat $out_file | jq '.jobs[].sync.lat_ns.percentile["99.000000"]')
+  echo "Sending FIO # $i value: $value"
+cat <<EOF | curl --data-binary @- http://$prometheus_server/metrics/job/fio/instance/$HOSTNAME
+# TYPE fio_latency counter
+fio_latency $value
+# TYPE fio_current_run counter
+fio_current_run $count
+EOF
+
+}
+
+while true
+do 
+
+  out_file=$r_dir/job-$i.json
+  rm -f $out_file
+
+  echo "Running FIO # $i"
+  fio --rw=write --ioengine=sync --fdatasync=1 --directory=/var/lib/etcd/fio --size=22m --bs=2300 --name=mytest --output-format=json+ >> $out_file
+
+  send_metrics $i
+  i=$((i+1))
+ 
 done
